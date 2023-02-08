e3b27affbe1dc59611d59a9fb5eba623d24fc045
Taranveer Singh <taranveersingh93@gmail.com>
2023-02-08 18:49:18 -0500

    Add another array and method

--- friends.js
+++ friends.js
@@ -2,3 +2,6 @@ var arrays = ["Rob", "Brad", "Sam", "George", "Sid"];
 console.log(arrays[2]);
 console.log(arrays[arrays.length-1]);
 console.log(arrays[0]);
+var moreArrays = ["Portland", "New York City", "Vancouver", "Prague", "Exton"]
+moreArrays.push("San Fransisco");
+console.log(moreArrays);
