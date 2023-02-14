bb6983f27a92f42e3869bc9795af929473b35e3d
Taranveer Singh <taranveersingh93@gmail.com>
2023-02-14 07:31:57 -0500

    Include the directions for the assignment

--- friends.js
+++ friends.js
@@ -1,3 +1,24 @@
+/*
+Directions:
+
+In your Mod 0 directory, create a new directory called pairing_2. 
+Inside of that directory, touch a file called friends. 
+Initialize a Git repository.
+Define a variable named arrays that stores an Array of 5 Strings with 
+friends names.
+Write code to print the third element.
+Write code to print the last element.
+Write code to print the first element.
+Run the file in the Terminal to verify your code is working as expected.
+Create a GitHub repository.
+Push your work from the local repository to the remote repository.
+Define another variable that stores an Array of 5 Strings, with names of 
+places to visit.
+Use one of the Array methods to do something to that new Array - you choose!
+Run the file in the Terminal to verify your code is working as expected.
+Push that new work up to the remote repository.
+*/
+
 var arrays = ["Rob", "Brad", "Sam", "George", "Sid"];
 console.log(arrays[2]);
 console.log(arrays[arrays.length-1]);
